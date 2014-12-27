746d71313a40b7dc58c5507ed861dcaef52dc810
James Rhodes <jrhodes@redpointsoftware.com.au>
2014-12-27 15:26:26 +0800

    Replace select() with poll() to catch broken write pipes

--- fd/fd.c
+++ fd/fd.c
@@ -9,6 +9,7 @@
 #include <errno.h>
 #include <stddef.h>
 #include <ancillary.h>
+#include <poll.h>
 
 // Get the original functionality back after PHP overrides it.
 #ifdef snprintf
@@ -26,14 +27,16 @@ PHP_FUNCTION(fd_select) {
   int *except_fd_list, except_fd_count;
   HashPointer pointer;
   
-  int i, a;
+  int i, a, t;
   zval** elem;
   
   fd_set read_fds;
   fd_set write_fds;
   fd_set except_fds;
   struct timeval timeout;
-  int ready;
+  int ready, value;
+  
+  struct pollfd* polls;
   
   zval *read_result_array;
   zval *write_result_array;
@@ -48,12 +51,14 @@ PHP_FUNCTION(fd_select) {
   read_fd_count = zend_hash_num_elements(Z_ARRVAL_P(read_array));
   read_fd_list = emalloc(sizeof(int) * read_fd_count);
   a = 0;
+  t = 0;
   for (
     zend_hash_internal_pointer_reset_ex(Z_ARRVAL_P(read_array), &pointer);
     zend_hash_get_current_data_ex(Z_ARRVAL_P(read_array), (void**)&elem, &pointer) == SUCCESS;
     zend_hash_move_forward_ex(Z_ARRVAL_P(read_array), &pointer)) {
     
     read_fd_list[a++] = Z_LVAL_PP(elem);
+    t++;
   }
   
   write_fd_count = zend_hash_num_elements(Z_ARRVAL_P(write_array));
@@ -65,6 +70,7 @@ PHP_FUNCTION(fd_select) {
     zend_hash_move_forward_ex(Z_ARRVAL_P(write_array), &pointer)) {
     
     write_fd_list[a++] = Z_LVAL_PP(elem);
+    t++;
   }
   
   except_fd_count = zend_hash_num_elements(Z_ARRVAL_P(except_array));
@@ -76,9 +82,10 @@ PHP_FUNCTION(fd_select) {
     zend_hash_move_forward_ex(Z_ARRVAL_P(except_array), &pointer)) {
     
     except_fd_list[a++] = Z_LVAL_PP(elem);
+    t++;
   }
   
-  FD_ZERO(&read_fds);
+  /*FD_ZERO(&read_fds);
   FD_ZERO(&write_fds);
   FD_ZERO(&except_fds);
   
@@ -87,24 +94,49 @@ PHP_FUNCTION(fd_select) {
   for (i = 0; i < except_fd_count; i++) { FD_SET(except_fd_list[i], &except_fds); }
   
   timeout.tv_sec = 0;
-  timeout.tv_usec = 0;
+  timeout.tv_usec = 0;*/
+  
+  // Use poll() first; this will tell us about closed pipes in the write FDs
+  // list (which select won't).
+  polls = malloc(sizeof(struct pollfd) * t);
+  for (i = 0; i < read_fd_count; i++) {
+    polls[i].fd = read_fd_list[i];
+    polls[i].events = POLLIN | POLLERR | POLLHUP | POLLRDHUP | POLLNVAL;
+  }
+  for (i = 0; i < write_fd_count; i++) {
+    polls[i + read_fd_count].fd = write_fd_list[i];
+    polls[i + read_fd_count].events = POLLERR | POLLHUP | POLLRDHUP | POLLNVAL;
+  }
+  for (i = 0; i < except_fd_count; i++) {
+    polls[i + read_fd_count + write_fd_count].fd = except_fd_list[i];
+    polls[i + read_fd_count + write_fd_count].events = POLLERR | POLLHUP | POLLRDHUP | POLLNVAL;
+  }
   
   ready = TEMP_FAILURE_RETRY(
-    select(
-      FD_SETSIZE,
-      &read_fds,
-      &write_fds,
-      &except_fds,
-      NULL));
+    poll(
+      polls,
+      t,
+      -1));
   
   array_init(return_value);
   add_assoc_long(return_value, "ready", ready);
   
   ALLOC_INIT_ZVAL(read_result_array);
   array_init(read_result_array);
-  for (i = 0; i < read_fd_count; i++) { 
-    if (FD_ISSET(read_fd_list[i], &read_fds)) {
-      add_index_bool(read_result_array, read_fd_list[i], 1);
+  for (i = 0; i < read_fd_count; i++) {
+    value = polls[i].revents;
+    if (value & POLLIN) {
+      add_index_string(read_result_array, read_fd_list[i], "ready", 1);
+    } else if (value & POLLERR) {
+      add_index_string(read_result_array, read_fd_list[i], "error", 1);
+    } else if (value & POLLHUP) {
+      add_index_string(read_result_array, read_fd_list[i], "error", 1);
+    } else if (value & POLLRDHUP) {
+      add_index_string(read_result_array, read_fd_list[i], "error", 1);
+    } else if (value & POLLNVAL) {
+      add_index_string(read_result_array, read_fd_list[i], "invalid", 1);
+    } else if (value != 0) {
+      add_index_string(read_result_array, read_fd_list[i], "unknown", 1);
     } else {
       add_index_bool(read_result_array, read_fd_list[i], 0);
     }
@@ -113,9 +145,18 @@ PHP_FUNCTION(fd_select) {
   
   ALLOC_INIT_ZVAL(write_result_array);
   array_init(write_result_array);
-  for (i = 0; i < write_fd_count; i++) { 
-    if (FD_ISSET(read_fd_list[i], &write_fds)) {
-      add_index_bool(write_result_array, write_fd_list[i], 1);
+  for (i = 0; i < write_fd_count; i++) {
+    value = polls[i + read_fd_count].revents;
+    if (value & POLLERR) {
+      add_index_string(write_result_array, write_fd_list[i], "error", 1);
+    } else if (value & POLLHUP) {
+      add_index_string(write_result_array, write_fd_list[i], "error", 1);
+    } else if (value & POLLRDHUP) {
+      add_index_string(write_result_array, write_fd_list[i], "error", 1);
+    } else if (value & POLLNVAL) {
+      add_index_string(write_result_array, write_fd_list[i], "invalid", 1);
+    } else if (value != 0) {
+      add_index_string(write_result_array, write_fd_list[i], "unknown", 1);
     } else {
       add_index_bool(write_result_array, write_fd_list[i], 0);
     }
@@ -125,8 +166,17 @@ PHP_FUNCTION(fd_select) {
   ALLOC_INIT_ZVAL(except_result_array);
   array_init(except_result_array);
   for (i = 0; i < except_fd_count; i++) { 
-    if (FD_ISSET(read_fd_list[i], &except_fds)) {
-      add_index_bool(except_result_array, except_fd_list[i], 1);
+    value = polls[i + read_fd_count + write_fd_count].revents;
+    if (value & POLLERR) {
+      add_index_string(except_result_array, except_fd_list[i], "error", 1);
+    } else if (value & POLLHUP) {
+      add_index_string(except_result_array, except_fd_list[i], "error", 1);
+    } else if (value & POLLRDHUP) {
+      add_index_string(except_result_array, except_fd_list[i], "error", 1);
+    } else if (value & POLLNVAL) {
+      add_index_string(except_result_array, except_fd_list[i], "invalid", 1);
+    } else if (value != 0) {
+      add_index_string(except_result_array, except_fd_list[i], "unknown", 1);
     } else {
       add_index_bool(except_result_array, except_fd_list[i], 0);
     }
@@ -243,6 +293,8 @@ PHP_FUNCTION(fd_pipe) {
     RETURN_FALSE;
   }
   
+  TRACE_CUSTOM("created native pipe (write) %d -> %d (read)", endpoint[1], endpoint[0]);
+  
   array_init(return_value);
   add_assoc_long(return_value, "read", endpoint[0]);
   add_assoc_long(return_value, "write", endpoint[1]);
@@ -302,6 +354,8 @@ PHP_FUNCTION(fd_control_pipe) {
     RETURN_FALSE;
   }
   
+  TRACE_CUSTOM("created socket pair (write) %d -> %d (read)", endpoint[1], endpoint[0]);
+  
   array_init(return_value);
   add_assoc_long(return_value, "read", endpoint[0]);
   add_assoc_long(return_value, "write", endpoint[1]);
@@ -316,6 +370,8 @@ PHP_FUNCTION(fd_control_writefd) {
   if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "ll", &pipe_fd, &fd_to_send) == FAILURE) {
     RETURN_FALSE;
   }
+  
+  TRACE_CUSTOM("writing FD %d using socket FD %d", fd_to_send, pipe_fd);
   
   if (ancil_send_fd(pipe_fd, fd_to_send) < 0) {
     RETURN_FALSE;
@@ -335,8 +391,10 @@ PHP_FUNCTION(fd_control_readfd) {
   }
   
   if (ancil_recv_fd(pipe_fd, &fd_to_recv) < 0) {
+    TRACE_CUSTOM("failed to read FD from %d", pipe_fd);
     RETURN_FALSE;
   } else {
+    TRACE_CUSTOM("read FD %d from socket FD %d", fd_to_recv, pipe_fd);
     RETURN_LONG(fd_to_recv);
   }
 }
